3c3b94061e3f44b528ad20dde54d6fdea3f67193
ishimori <ishimori@probsc.jp>
2021-02-28 14:37:56 +0900

    2-3

--- src/components/pages/CalendarBoard/index.tsx
+++ src/components/pages/CalendarBoard/index.tsx
@@ -1,45 +1,26 @@
 import React from "react";
 import GenericTemplate from "../../templates/GenericTemplate";
 import { GridList } from "@material-ui/core";
+import dayjs from "dayjs";
+import "dayjs/locale/ja";
 import "./style.scss";
 
-const calendar = [
-  "29",
-  "30",
-  "10月1日",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "11",
-  "12",
-  "13",
-  "14",
-  "15",
-  "16",
-  "17",
-  "18",
-  "19",
-  "20",
-  "21",
-  "22",
-  "23",
-  "24",
-  "25",
-  "26",
-  "27",
-  "28",
-  "29",
-  "30",
-  "31",
-  "11月1日",
-  "2",
-];
+dayjs.locale("ja");
+
+const createCalendar = () => {
+  const firstDay = dayjs().startOf("month");
+  const firstDayIndex = firstDay.day();
+
+  return Array(35)
+    .fill(0)
+    .map((val, index) => {
+      const diffFromFirstDay = index - firstDayIndex;
+      const day = firstDay.add(diffFromFirstDay, "day");
+      console.log(day);
+      return day;
+    });
+};
+const calendar = createCalendar();
 
 const CalendarBoard = () => {
   return (
@@ -47,8 +28,8 @@ const CalendarBoard = () => {
       <div className="container">
         <GridList className="grid" cols={7} spacing={0} cellHeight="auto">
           {calendar.map((c) => (
-            <li>
-              <div className="element">{c}</div>
+            <li key={c.toISOString()}>
+              <div className="element">{c.format("D")}</div>
             </li>
           ))}
         </GridList>
