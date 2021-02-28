15fa53880a5737add4484a0a7d5776e87d5fedb3
ishimori <ishimori@probsc.jp>
2021-02-28 23:04:07 +0900

    2-6まで終了

--- src/components/pages/CalendarElement/index.tsx
+++ src/components/pages/CalendarElement/index.tsx
@@ -1,22 +1,34 @@
 import React from "react";
 import "./style.scss";
 import { Typography } from "@material-ui/core";
+import dayjs from "dayjs";
+// ==========ここから変更する==========
+import { isSameMonth, isFirstDay, isSameDay } from "../../../services/calendar";
 
 const CalendarElement = ({ day }: any) => {
-  // 文字列のフォーマットをどうするか
-  const isFirstDay = day.date() === 1;
+  // 当日かどうか判断
+  const today = dayjs();
+
+  // 今月以外をグレーダウン
+  const isCurrentMonth = isSameMonth(day, today);
+  const textColor = isCurrentMonth ? "textPrimary" : "textSecondary";
+
   // 月の最初だけ月情報をつける
-  const format = isFirstDay ? "M月D日" : "D";
+  const format = isFirstDay(day) ? "M月D日" : "D";
+
+  // 当日か判断
+  const isToday = isSameDay(day, today);
 
   return (
     <div className="element">
       <Typography
         className="date"
+        color={textColor}
         align="center"
         variant="caption"
         component="div"
       >
-        {day.format(format)}
+        <span className={isToday ? "today" : ""}>{day.format(format)}</span>
       </Typography>
     </div>
   );
